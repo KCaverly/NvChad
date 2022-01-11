f006cebaa5249674dcd20ad65f7c4e06676aca7f
KCaverly <kylebcaverly@gmail.com>
2022-01-10 23:30:15 -0500

    updated keymaps

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -13,10 +13,29 @@ customPlugins.add(function(use)
 
  -- MAPPINGS
  local map = require("core.utils").map
-
+ 
  -- Disable Arrow Keys
  map("n", "<Down>", "<NOP>")
  map("n", "<Up>", "<NOP>")
  map("n", "<Left>", "<NOP>")
  map("n", "<Right>", "<NOP>")
 
+ -- Misc
+ map("n", "Y", "y$")
+
+ -- Keep Search Items centered
+ map("n", "n", "nzzzv")
+ map("n", "N", "Nzzzv")
+ map("n", "J", "mzJ`z")
+
+ -- Undo Break Points
+ map("i", ",", ",<c-g>u")
+ map("i", ".", ".<c-g>u")
+ map("i", "!", "!<c-g>u") 
+ map("i", "?", "?<c-g>u")
+
+ -- Moving Text Around
+ map("i", "<C-j>", "<esc>:m .+1<CR>==i")
+ map("i", "<C-k>", "<esc>:m .-2<CR>==i")
+ map("n", "<leader>k", ":m .-2<CR>==")
+ map("n", "<leader>j", ":m .+1<CR>==")
