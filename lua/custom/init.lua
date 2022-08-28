418ac59b551a54dec0db784990cb479d6f50dbcf
KCaverly <kylebcaverly@gmail.com>
2022-08-28 19:13:16 -0400

    fixed clipboard

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,14 +1,17 @@
 -- example file i.e lua/custom/init.lua
 
 -- load your globals, autocmds here or anything .__.
-vim.api.nvim_create_autocmd("TermOpen", {command = "startinsert"})
+vim.api.nvim_create_autocmd("TermOpen", { command = "startinsert" })
 
 -- Relative Number Settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- UFO Fold Settings
-vim.opt.foldcolumn = '0'
+vim.opt.foldcolumn = "0"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = -1
 vim.opt.foldenable = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
