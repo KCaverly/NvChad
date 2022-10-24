a930c54a33921e9977df4f0d5ea5f500d77ba647
KCaverly <kylebcaverly@gmail.com>
2022-10-24 11:52:31 -0400

    updated autoformatting and colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,3 +1,14 @@
+require("gruvbox").setup {
+  overrides = {
+    TabLineFill = { bg = "#282828" },
+    TabLineSel = { bg = "#3c3836" },
+    SignColumn = { bg = "#282828" },
+    GruvboxRedSign = { bg = "#282828" },
+    GruvboxGreenSign = { bg = "#282828" },
+    GruvboxAquaSign = { bg = "#282828" },
+  },
+}
+
 local colorscheme = "gruvbox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
