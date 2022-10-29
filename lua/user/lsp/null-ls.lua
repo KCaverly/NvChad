cb6108b59c3389d676f36e244184be51743f9b1b
KCaverly <kylebcaverly@gmail.com>
2022-10-29 16:42:50 -0400

    added spellcheck

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -7,6 +7,7 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local completion = null_ls.builtins.completion
 
 null_ls.setup({
 	debug = false,
@@ -16,6 +17,11 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
     formatting.isort,
     diagnostics.mypy.with({ extra_args = {"--ignore-missing-imports", "--follow-imports=silent"}}),
-    diagnostics.flake8.with({ extra_args = {"--max-line-length", "88", "--ignore=E203"}})
+    diagnostics.flake8.with({ extra_args = {"--max-line-length", "88", "--ignore=E203"}}),
+
+    -- General
+    completion.spell,
+    diagnostics.cspell
+
 	},
 })
